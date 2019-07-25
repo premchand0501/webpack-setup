44ec4faa3284737ea6d54e524d9813781784d4f2
Prem <premsahni0501@gmail.com>
2019-07-25 13:52:03 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('style.min.css');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './src/app.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    // publicPath: './src/assets/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              context: path.resolve(__dirname, 'src/'),
+              useRelativePaths: true,
+              publicPath: '../'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: extractCSS.extract([
+          'css-loader',
+          'sass-loader',
+          'postcss-loader'
+        ])
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    extractCSS
+  ]
+}
